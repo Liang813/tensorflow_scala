a00ea9b7701c4f057d5d98c10975ce2f3519c314
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-03-14 20:57:03 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import tensorflow as tf   
+import tsensor
+
 try:
   tf.convert_to_tensor(tf.TensorShape(()))
-  tf.random_normal(1)
+  with tsensor.clarify():
+    tf.random_normal(1)
 except Exception as e:
   print(e)
