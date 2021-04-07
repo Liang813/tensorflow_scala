c35d2de508a98ee4a7c210167e1deac0125e3a1f
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-04-07 15:45:11 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import tensorflow as tf   
-
+import traceback
 try:
   tf.convert_to_tensor(tf.TensorShape(()))
   tf.random_normal(1)
 except Exception as e:
-  print("ValueError")
-  print(e)
+  # print("ValueError")
+  # print(e)
+  traceback.print_exc()
