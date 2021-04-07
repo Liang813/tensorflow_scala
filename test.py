09d39ddd7401bdcaa380f4b9023aebfbf7d0efe1
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-04-07 15:55:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,5 @@ try:
   tf.convert_to_tensor(tf.TensorShape(()))
   tf.random_normal(1)
 except Exception as e:
-  # print("ValueError")
-  # print(e)
-  traceback.print_exc()
+  print(traceback.print_exc())
+  
